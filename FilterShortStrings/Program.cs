cb5b5c1aa3a4beda9c149a5ba9a0910032fcfbf4
Timur Cherenkov <ticher26@gmail.com>
2024-04-09 19:47:34 +0300

    Внесены изменения в код

--- FilterShortStrings/Program.cs
+++ FilterShortStrings/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+    Console.WriteLine("Введите строки:");
+    string input = Console.ReadLine()!;
+    string[] inputArray = input.Split(',')!;
+    string[] resultArray = FilterShortStrings(inputArray);
+    Console.WriteLine("Результат: ");
+    foreach (string str in resultArray)
+    {
+        Console.WriteLine(str);
+    }
+    
+    static string[] FilterShortStrings(string[] inputArray)
+    {
+        int count = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+        string[] resultArray = new string[count];
+        int index = 0;
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                resultArray[index] = str;
+                index++;
+            }
+        }
+
+        return resultArray;
+    }
